463e5490ecf3ae6ecf50d01aa0429c3e93082fa7
Tom Anderson <me@thomas-anderson.net>
2020-04-23 19:23:26 +0100

    Changing site serving to Flask

---
+++ core.py
@@ -0,0 +1,26 @@
+# Import OS to get environment variables
+import os
+import datetime
+
+# Get Flask and associated modules
+from flask import Flask, request, jsonify, abort, render_template
+
+# Import Google API Client
+from googleapiclient.discovery import build
+
+# Define the Flask app name from the filename
+site = Flask(__name__)
+
+# Get environment, and default to Development
+env = os.environ.get('appenv', 'Local')
+rev = os.environ.get('K_REVISION', 'No revision')
+
+################
+# Begin Routes #
+################
+
+# Root
+@site.route('/')
+def main():
+#    return jsonify(revision=rev, environment=env)
+    return render_template('index.html')

0374be575330a92c898fffe6cd75b3d1aa8582d3
Tom Anderson <me@thomas-anderson.net>
2020-07-13 19:34:53 +0100

    Starting to add CSS

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,6 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    process.env.MINIMISE  === 'yes' ? require('cssnano')({preset: 'default',}) : false,
+    process.env.MINIMISE  === 'yes' ? require('cssnano')({preset: 'default',}) : false, // This will only use cssnano (to minify css) if MINIMISE is set to 'yes'
   ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-    // This will only purge if _PURGE === true
+    // This will only purge if PURGE is set to 'yes'
     enabled: process.env.PURGE === 'yes' ? true : false ,
     content: ['./source/**/*.html','./source/**/*.liquid'],
   },

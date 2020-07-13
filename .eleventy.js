a1a4b15024fef086e1900b63a90f7da91cecf614
Tom Anderson <me@thomas-anderson.net>
2020-07-13 09:42:27 +0100

    Add liquid templates.

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+
+  // Add a filter using the Config API  
+  // Do things.
+
+  // Return the config object.
+  return {
+    dir: {
+      input: "source",
+      output: "build"
+    }
+  };
+};

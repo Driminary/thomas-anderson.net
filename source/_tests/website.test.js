a3cd11f4483736191b4b544505874b8c76fa02e9
Tom Anderson <me@thomas-anderson.net>
2020-07-21 20:21:11 +0100

    Page consolidation

--- source/_tests/website.test.js
+++ source/_tests/website.test.js
@@ -17,18 +17,4 @@ describe('Functional', () => {
     await expect(imgs).toBe("/img/mesmaller.jpg")
   });
 
-  it('Contains link to "About" page.', async () => {
-    const about = await page.$x("//a[contains(., 'About')]")
-    expect(about.length > 0)
-  });
-
-  it('Can navigate to "About" page via navbar.', async () => {
-    const about = await page.$x("//a[contains(., 'About')]")
-    await Promise.all([
-      about[0].click(),
-      page.waitForNavigation({ waitUntil: 'networkidle0' }),
-    ]);
-    await expect(page.title()).resolves.toMatch('Tom Anderson - About')
-  });
-
 });

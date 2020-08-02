4fab38c07ec181bba067c491ec9909cd48f8eeb2
Thomas Anderson <me@thomas-anderson.net>
2020-08-02 16:41:35 +0100

    :art: Refactor API data file
    In preparation for post.

--- source/_data/devtoapi.js
+++ source/_data/devtoapi.js
@@ -1,11 +1,9 @@
 var axios = require("axios");
-
 var api_url = process.env.DEVTO_APIURL || "https://dev.to/api/articles?username=ndsn";
 
 module.exports = (async () => {
   try {
     let response = await axios.get(api_url);
-
     return { feed: api_url, posts: response.data };
   } catch (e) {
     console.error(e);

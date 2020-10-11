4539cfdc1ba59c118d3063412328c30e2c13d287
Tom Anderson <me@thomas-anderson.net>
2020-10-11 15:32:26 +0000

    Adding custom link click events in Plausible

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,9 @@ module.exports = function(eleventyConfig) {
   // Copy `img/` to `_build/img`
   eleventyConfig.addPassthroughCopy("source/img")
 
+  // Copy `js/` to `_build/js`
+  eleventyConfig.addPassthroughCopy("source/js")
+
   // Copy built css 'css/bundle.css' to '_build/css/bundle.css'
   eleventyConfig.addPassthroughCopy('source/css/bundle.css')
 

ca2807d4d0081dcc2157e0959e2abe96b4184f90
Tom Anderson <me@thomas-anderson.net>
2021-12-12 20:27:56 +0000

    Move to Tailwind CSS 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-  purge: {
-    // This will only purge if PURGE is set to 'yes'
-    enabled: process.env.PURGE === 'yes' ? true : false ,
-    content: ['./source/**/*.html','./source/**/*.liquid'],
-  },
+  content: [
+    './source/**/*.html',
+    './source/**/*.liquid'
+  ],
   theme: {
     extend: {
         colors: {
-        'anderson-blue': '#07689F',
         'deep-gray-100': '#21374b',
         'deep-gray-200': '#162533'
       }
